ea851a8f51bd0c60db702e048eff7925cfc06601
skulmoon <skulmoon4ikwork@gmail.com>
2024-12-28 23:13:12 +0300

    Test2

--- Data/Scripts/Entities/Enemy/EnemyFabric/EnemyArea.cs
+++ Data/Scripts/Entities/Enemy/EnemyFabric/EnemyArea.cs
@@ -8,22 +8,22 @@ public partial class EnemyArea : Area2D
 
     public void PlaceEnemy(Enemy enemy)
     {
-            AddChild(enemy);
-            Vector2 collisionSize = ((RectangleShape2D)GetNode<CollisionShape2D>("CollisionShape2D").Shape).Size;
-            enemy.Visible = true;
-            _numberOfEnemy++;
-            enemy.Label.Text = _numberOfEnemy.ToString();
-            while (true)
-            {
-                enemy.Position = new Vector2(
-                    (float)GD.RandRange(-collisionSize.X / 2, collisionSize.X / 2),
-                    (float)GD.RandRange(-collisionSize.Y / 2, collisionSize.Y / 2)
-                );
-                GD.Print(enemy.Position);
-                var collide = enemy.TestMove(enemy.GetTransform(), new Vector2(10, 0), null, 0, true);
-                GD.Print(collide);
-                if (collide)
-                    break;
+        AddChild(enemy);
+        Vector2 collisionSize = ((RectangleShape2D)GetNode<CollisionShape2D>("CollisionShape2D").Shape).Size;
+        enemy.Visible = true;
+        _numberOfEnemy++;
+        enemy.Label.Text = _numberOfEnemy.ToString();
+        while (true)
+        {
+            enemy.Position = new Vector2(
+                (float)GD.RandRange(-collisionSize.X / 2, collisionSize.X / 2),
+                (float)GD.RandRange(-collisionSize.Y / 2, collisionSize.Y / 2)
+            );
+            GD.Print(enemy.Position);
+            var collide = enemy.TestMove(enemy.GetTransform(), new Vector2(0, 0.001f), null, 0, true);
+            GD.Print(collide);
+            if (collide)
+                break;
             }
     }
 }
